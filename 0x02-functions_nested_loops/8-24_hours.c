28bde6e5d566187dc981a3063070d053201a9fe8
Nathaniel Solomon <nathaniel.o.solomon@gmail.com>
2022-06-07 15:31:23 -0700

    buga won

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,11 @@
 #include "main.h"
+
 /**
- * Jack Bauer - Print every minute of the day of Jack Bauer
+ * jack_bauer - Print every minute of the day of Jack Bauer
  * Description: Start from 00:00 to 23:59
  */
+
+
 void jack_bauer(void)
 {
 	int x = 0;
