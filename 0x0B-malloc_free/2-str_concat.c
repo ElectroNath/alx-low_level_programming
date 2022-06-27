cd9c8391e356e0d27d89282ac5467068586e9ad0
Nathaniel Solomon <nathaniel.o.solomon@gmail.com>
2022-06-27 11:32:53 -0700

    Memory Allocation Page

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,4 +33,20 @@ char *str_concat(char *s1, char *s2)
 	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
 	starts1 = new_str;
 	if (new_str == NULL)
-
+		return (NULL);
+	for (; i < (lens1 + lens2); i++)
+	{
+		if (i < lens1)
+		{
+			new_str[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[i] = *s2;
+			s2++;
+		}
+	}
+	new_str[i] = '\0';
+	return (starts1);
+}
